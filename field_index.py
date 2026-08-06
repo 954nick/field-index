673af7b35b96a53bfbd2ea5b267f1e46c93bc317
nickeckerson <231999035+nickeckerson@users.noreply.github.com>
2026-08-06 16:37:19 -0400

    Store the background color in a reusable variable

--- field_index.py
+++ field_index.py
@@ -7,11 +7,14 @@ version = 0.1
 
 app_title = f"{app_name} v{version}"
 
+# Define application colors
+background_color = "#17191C"
+
 # Create and configure the application window 
 app = tk.Tk()
 app.title(app_title)
 app.geometry("1500x900")
-app.configure(bg="#17191C")
+app.configure(bg=background_color)
 
 # Build paths to application assets
 project_folder = Path(__file__).parent
@@ -23,11 +26,9 @@ logo_image = tk.PhotoImage(
     format="svg -scaletowidth 350"
     )
 title_label = tk.Label(app, image=logo_image)
-title_label.configure(bg="#17191C")
+title_label.configure(bg=background_color)
 title_label.pack()
-
 
 # Start the Field Index application
 app.mainloop()
-
 
