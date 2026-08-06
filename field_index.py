a314b2d277807f4b47282aaeac053c584df96281
nickeckerson <231999035+nickeckerson@users.noreply.github.com>
2026-08-06 18:33:40 -0400

    Validate selected save files and add color-coded feedback

--- field_index.py
+++ field_index.py
@@ -11,6 +11,7 @@ app_title = f"{app_name} v{version}"
 # Define application colors
 background_color = "#17191C"
 primary_color = "#42E478"
+error_color = "#FF0000"
 
 # Select a CFB27 Dynasty save file
 def select_save_file():
@@ -20,7 +21,19 @@ def select_save_file():
     if selected_file:
         print(selected_file)
         dynasty_file_name = Path(selected_file).name
-        save_status_label.configure(text=dynasty_file_name)
+
+        # Read the selected save-file header
+        with open(selected_file, "rb") as opened_save_file:
+            save_header = opened_save_file.read(8)
+            print(save_header)
+            if save_header == b"FBCHUNKS":
+                print("Valid CFB27 save")
+                save_status_label.configure(text=dynasty_file_name, fg=primary_color)
+            else: 
+                print("Invalid file type")
+                save_status_label.configure(text="Invalid file type",
+                fg=error_color
+                )
 
 # Create and configure the application window 
 app = tk.Tk()
@@ -32,7 +45,7 @@ app.configure(bg=background_color)
 project_folder = Path(__file__).parent
 logo_path = project_folder / "assets" / "field_index_logo.svg"
 
-# Load and display the application logo
+# Load and display the application logo 
 logo_image = tk.PhotoImage(
     file=logo_path,
     format="svg -scaletowidth 350"
@@ -53,7 +66,6 @@ save_status_label = tk.Label(app,
     text="No save file selected")
 save_status_label.configure(bg=background_color, fg=primary_color)
 save_status_label.pack(pady=20)
-
 
 # Start the Field Index application
 app.mainloop()
